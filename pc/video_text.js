f65a679de8fe8a04db77b169bb9b1cf249efcc42
xxoo591 <62453317+xxoo591@users.noreply.github.com>
2020-03-21 10:34:48 +0800

    Create video_text.js

---
+++ pc/video_text.js
@@ -0,0 +1,4 @@
+document.writeln("<dd>推荐：<span>内页文字广告位出租</span></dd>");
+document.writeln("<dd>推荐：<span>内页文字广告位出租</span></dd>");
+document.writeln("<dd>推荐：<span>内页文字广告位出租</span></dd>");
+document.writeln("<dd>推荐：<span>内页文字广告位出租</span></dd>");
